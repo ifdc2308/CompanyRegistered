409194a51614d93c00f0769ee60893b8588ab8c7
ifdc2308 <italodorea123@gmail.com>
2024-03-17 11:49:17 -0300

    input-and api-data

---
+++ main.js
@@ -0,0 +1,39 @@
+// Esta função será chamada com os dados da API
+function handleResponse(data) {
+    console.log(data);
+    alert(console.error(data));// Aqui você pode ver os dados recebidos no console para depuração
+
+    // Estruture os dados recebidos em um formato mais legível ou conforme necessário
+    const dadosFormatados = {
+        'CNPJ': data.cnpj,
+        'Nome': data.nome,
+        // Verifique se 'atividade_principal' existe para evitar erros
+        'Atividade Principal': data.atividade_principal ? data.atividade_principal.map(ap => ap.text).join(', ') : '',
+        // Adicione mais campos conforme necessário
+    };
+
+    // Chama a função para atualizar o HTML com os dados formatados
+    renderizarDados(dadosFormatados);
+}
+
+// Esta função atualiza o HTML com os dados fornecidos
+function renderizarDados(dados) {
+    const container = document.getElementById('resultado');
+    container.innerHTML = ''; // Limpa o conteúdo anterior
+
+    // Itera sobre cada par chave-valor no objeto de dados e os adiciona ao contêiner
+    for (const [chave, valor] of Object.entries(dados)) {
+        const elemento = document.createElement('p');
+        elemento.textContent = `${chave}: ${valor}`;
+        container.appendChild(elemento);
+    }
+}
+
+document.getElementById('SubmitButtonSearch').addEventListener('click', function () {
+    alert("Botão foi clicado!");
+
+    const cnpj = document.getElementById('cnpjInput').value;
+    const script = document.createElement('script');
+    script.src = `https://receitaws.com.br/v1/cnpj/${cnpj}?callback=handleResponse`;
+    document.body.appendChild(script);
+});
